b0d99cd1c2ef948ec8e32fc1d1becb3bcfb512b4
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-12 01:49:46 +0300

    app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as LocalAuth from 'expo-local-authentication'
+
 import Auth from './src/screens/AuthScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
 
 export default function App() {
+  const [isBiometric, setIsBiometric] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const compatible = await LocalAuth.hasHardwareAsync();
+      setIsBiometric(compatible);
+    })();
+  });
+
+  function onAuthenticate () {
+    const auth = LocalAuth.authenticateAsync({
+      promptMessage: 'Authenticate',
+      fallbackLabel: 'Enter Password',
+    });
+    auth.then(result => {
+      setIsAuth(result.success);
+        console.log(result);
+    }
+    );
+  }
+  
+
   return (
     <View style={styles.container}>
-       {/*  <Auth/> */}
-       <PaymentScreen/>
+      { isAuth
+        ?  <PaymentScreen/> 
+        :   <Auth/>
+      }
     </View> 
   );
 }
