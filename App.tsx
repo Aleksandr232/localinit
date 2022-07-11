7d4fc7f9b88419cd69cd4727022548c769d7e809
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-11 17:57:07 +0300

    app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Auth from './src/screens/AuthScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Auth/>
     </View>
   );
 }
---
+++ src/screens/AuthScreen.tsx
@@ -0,0 +1,53 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+const myImage = require('../../assets/1.png');
+
+export default function Auth(){
+    return(
+        <View>
+            <Image source={myImage} style={styles.image}/>
+            <Text style={styles.title}>sPAY</Text>
+            <Text style={styles.description}>Оплата</Text>
+            <TouchableOpacity
+               /*  onPress={onAuthnticate} */
+                style={styles.btn}>
+                 <Text style={styles.login}>Войти</Text>   
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    btn: {
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        backgroundColor: '#0893FC',
+        padding: 10,
+        borderRadius: 5,
+    },
+    image: {
+        width: 400,
+        height: 461,
+    },
+    login: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: '400',
+        marginVertical: 30,
+        textAlign: 'center',
+    },
+    description: {
+        fontSize: 18,
+        color: 'gray',
+        textAlign: 'center',
+        marginHorizontal: 20,
+        marginBottom: 50,
+    }
+})
