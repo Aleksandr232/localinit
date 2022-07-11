0f28b3c116237f02b88b5d1357762e091c8d9681
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-12 00:46:21 +0300

    PAYMENT

---
+++ src/screens/PaymentScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text, StyleSheet, Image, TouchableOpacity, TextInput, SafeAreaView, Button } from "react-native";
+
+
+const PaymentScreen=()=>{
+    return(
+        
+    )
+}
+
+export default PaymentScreen
